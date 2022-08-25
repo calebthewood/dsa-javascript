9185d739d6909cf9f65658ef0a7e66ebd37ac21b
calebthewood <calebwood.cs@gmail.com>
2022-08-25 11:38:05 -0700

    Sliding Window

--- JS-MasterClass/MultiplePointers.js
+++ JS-MasterClass/MultiplePointers.js
@@ -19,3 +19,39 @@ function sumZero(arr) {
 }
 
 console.log("sumsZero: ", sumZero([-4, -3, -2, -1, 1, 2, 3, 4, 5]));
+
+function countValues(arr) {
+  const len = arr.length;
+  let left = 0;
+  let right = left + 1;
+  let uniqueSum = 0;
+
+  while (left < len) {
+    if (arr[left] !== arr[right]) {
+      right = left + 1;
+      if (right < len && arr[left] !== arr[right]) {
+        uniqueSum++;
+      }
+    } else {
+      left++;
+    }
+  }
+  return uniqueSum;
+}
+
+function betterCountValues(arr) {
+  const len = arr.length;
+  if (!len) return 0;
+
+  let i = 0;
+  let j = 1;
+
+  while (j < len) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+    j++;
+  }
+  return i + 1;
+}
