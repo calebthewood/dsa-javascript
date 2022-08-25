f758dbb4d8d3604802c54d65254db80ee7e76662
calebthewood <calebwood.cs@gmail.com>
2022-08-25 11:44:05 -0700

    Divide & Conquer Intro

--- JS-MasterClass/DivideConquer.js
+++ JS-MasterClass/DivideConquer.js
@@ -6,18 +6,22 @@ Divide and Conquer Algos
 - useful in searching, binary searching especially
 */
 
+/**
+ * O(log(n))
+ */
 function binarySearch(array, val) {
 
   let min = 0;
   let max = array.length - 1;
 
   while (min <= max) {
+    console.log("hit")
     let middle = Math.floor((min + max) / 2);
     let currentElement = array[middle];
 
-    if (array[middle < val]) {
+    if (currentElement < val) {
       min = middle + 1;
-    } else if (array[middle] > val) {
+    } else if (currentElement > val) {
       max = middle + 1;
     } else {
       return middle;
@@ -25,3 +29,6 @@ function binarySearch(array, val) {
   }
   return -1;
 }
+
+
+console.log(binarySearch([1,2,3,4,5,6,7,8,9,10,11,12,14,50,90,99,100,1000], 1000))
