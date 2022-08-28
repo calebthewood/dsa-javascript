66420e4bc2a8891edf91f5ef0eda16497246cb71
calebthewood <calebwood.cs@gmail.com>
2022-08-28 11:32:18 -0700

    Linear Search

---
+++ JS-MasterClass/LinearSearch.js
@@ -0,0 +1,25 @@
+"use strict"
+
+/**
+ * Time Complexity O(n)
+ * Space Complexity O(1)
+ * @param {[*]} array
+ * @param {Number} item
+ * @returns {Number} Index
+ */
+function linearSearch(array, item) {
+  const len = array.length;
+  for (let i = 0; i < len; i++) {
+    if (array[i] === item) {
+      return i
+    }
+  }
+  return -1;
+}
+
+console.log(linearSearch([10, 15, 20, 25, 30], 15)) // 1
+console.log(linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 4)) // 5
+console.log(linearSearch([100], 100)) // 0
+console.log(linearSearch([1,2,3,4,5], 6)) // -1
+console.log(linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 10)) // -1
+console.log(linearSearch([100], 200)) // -1
