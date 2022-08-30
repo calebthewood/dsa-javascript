b043c1115179905a9fd0a1a06bf7e7b923503b64
calebthewood <calebwood.cs@gmail.com>
2022-08-30 15:41:40 -0700

    BubbleSort

--- JS-MasterClass/SortingAlgos.js
+++ JS-MasterClass/SortingAlgos.js
@@ -1,1 +1,26 @@
-"use strict"
+"use strict";
+
+function swap(arr, idx1, idx2) {
+  var temp = arr[idx1];
+  arr[idx1] = arr[idx2];
+  arr[idx2] = temp;
+}
+
+function bubbleSort(array) {
+  const len = array.length;
+
+  for (let i = len; i > 0; i--) {
+    let noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        swap(array, j + 1, j);
+        console.log("Array:   ", array);
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return array;
+}
+
+console.log(bubbleSort([1, 5, 50, 99, 13, 56, 23, 7, 3, 4, 1, 9, 2]));
