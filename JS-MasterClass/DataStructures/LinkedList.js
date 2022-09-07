4cfbf1d003f79ee5e1f81761dbbbc2a9731525d0
calebthewood <calebwood.cs@gmail.com>
2022-09-07 12:00:48 -0700

    LinkedList - wip

---
+++ JS-MasterClass/DataStructures/LinkedList.js
@@ -0,0 +1,29 @@
+"use strict";
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SingleLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = null;
+  }
+
+  push(val) {
+    const node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length += 1;
+    return this;
+  }
+}
