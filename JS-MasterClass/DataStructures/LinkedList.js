39a7eb58e97a254303683dbd96943a2efdc80eb0
calebthewood <calebwood.cs@gmail.com>
2022-09-07 17:28:01 -0700

    Completes Linked List

--- JS-MasterClass/DataStructures/LinkedList.js
+++ JS-MasterClass/DataStructures/LinkedList.js
@@ -7,7 +7,7 @@ class Node {
   }
 }
 
-class SingleLinkedList {
+class SinglyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
@@ -17,7 +17,7 @@ class SingleLinkedList {
   print() {
     const values = [];
     let current = this.head;
-    while (current.next) {
+    while (current) {
       values.push(current.val);
       current = current.next;
     }
@@ -79,13 +79,79 @@ class SingleLinkedList {
     this.length += 1;
     return this;
   }
+
+  get(index) {
+    if (index >= this.length || index < 0) return null;
+    if (index === 0) return this.head;
+    if (index === this.length - 1) return this.tail;
+
+    let current = this.head;
+    for (let i = 0; i <= index; i++) {
+      current = current.next;
+    }
+    return current;
+  }
+
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, val) {
+    if (index > this.length || index < 0) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
+
+    let node = new Node(val);
+    let previous = this.get(index - 1);
+    let current = previous.next;
+    node.next = current;
+    previous.next = node;
+    return true;
+  }
+
+  remove(index) {
+    if (!this.head) return null; // empty list
+    if (index < 0 || index >= this.length) return false; //invalid index
+    if (index === 0) return this.shift(); // remove head
+    if (index === this.length - 1) return this.pop(); //remove tail
+
+    let previous = this.get(index - 1);
+    let removed = previous.next;
+    previous.next = removed.next;
+    return removed;
+  }
+
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this
+  }
+
 }
 
-let list = new SingleLinkedList();
+let list = new SinglyLinkedList();
 list.push("hello");
 list.push(" ");
 list.push("world");
 list.push("!");
-console.log(list.print());
 list.unshift("well ");
+list.set(1, " $@#$ ");
 console.log(list.print());
+list.reverse();
+console.log(list.print());
+
