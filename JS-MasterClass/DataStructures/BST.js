469ee683f720a412592414b4004c9ff80d2d8ce0
calebthewood <calebwood.cs@gmail.com>
2022-09-09 18:33:52 -0700

    Add Simple Binary Search Tree

---
+++ JS-MasterClass/DataStructures/BST.js
@@ -0,0 +1,90 @@
+"use strict";
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  printDFS() {
+    let stack = [this.root];
+    let values = [];
+    let current = stack.pop();
+    while (current) {
+      values.push(current.val);
+      if (current.left) stack.push(current.left);
+      if (current.right) stack.push(current.right);
+      current = stack.pop();
+    }
+    console.log(values.join(" "));
+  }
+
+  insert(val) {
+    let node = new Node(val);
+    if (!this.root) {
+      this.root = node;
+      return this;
+    }
+    let current = this.root;
+    while (current) {
+      if (val === current.val) return;
+      if (val < current.val) {
+        if (!current.left) {
+          current.left = node;
+          return this;
+        }
+        current = current.left;
+      } else if (val > current.val) {
+        if (!current.right) {
+          current.right = node;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+
+  find(val) {
+    if (!this.root) return null;
+    let current = this.root;
+
+    while (current) {
+      if (val < current.val) {
+        current = current.left;
+      } else if (val > current.val) {
+        current = current.right;
+      } else if (val === current.val) {
+        return current;
+      }
+    }
+    return null;
+  }
+
+  contains(val) {
+    if (this.root === null) return false;
+    let current = this.root;
+    while (current) {
+      if (val < current.val) {
+        current = current.left;
+      } else if (val > current.val) {
+        current = current.right;
+      } else if (val === current.val) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+}
+
+let tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.insert(8).insert(11).insert(3).insert(15).insert(1).insert(50).insert(9);
+tree.printDFS();
+console.log(tree.contains(0));
