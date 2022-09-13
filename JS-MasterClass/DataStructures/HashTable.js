68097dc4a2ea6d7b4b2f79a21f42cb9a39fb698b
calebthewood <calebwood.cs@gmail.com>
2022-09-12 22:34:31 -0700

    Wrap up HashTables

--- JS-MasterClass/DataStructures/HashTable.js
+++ JS-MasterClass/DataStructures/HashTable.js
@@ -22,7 +22,7 @@ function hashV2(key, arrayLen) {
 }
 
 class HashTable {
-  constructor(size=53) {
+  constructor(size = 53) {
     this.keyMap = new Array(size);
   }
 
@@ -31,19 +31,26 @@ class HashTable {
     let WEIRD_PRIME = 31;
     for (let i = 0; i < Math.min(key.length, 100); i++) {
       let char = key[i];
-      let value = char.charCodeAt(0) - 96
+      let value = char.charCodeAt(0) - 96;
       total = (total * WEIRD_PRIME + value) % this.keyMap.length;
     }
     return total;
   }
-
+  // susceptible to duplicate keys
   set(key, val) {
     let index = this._hash(key);
+    let isDuplicate = false;
     if (!this.keyMap[index]) {
       this.keyMap[index] = [];
-    }
-    this.keyMap[index].push([key, val])
-    return index
+    } // loop checks for duplicate keys
+    for (let i = 0; i < this.keyMap[index].length; i++) {
+      if (key === this.keyMap[index][i][0]) {
+        this.keyMap[index][i][1] = val;
+        isDuplicate = true;
+      }
+    } // clunky way to do this, but c'est la vie
+    if (!isDuplicate) this.keyMap[index].push([key, val]);
+    return index;
   }
 
   get(key) {
@@ -57,16 +64,49 @@ class HashTable {
     }
   }
 
+  keys() {
+    const keys = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        if (this.keyMap[i].length > 1) {
+          for (let j = 0; j < this.keyMap[i].length; j++) {
+            if (!keys.includes(this.keyMap[i][j][1])) {
+              keys.push(this.keyMap[i][j][0]);
+            }
+          }
+        } else {
+          keys.push(this.keyMap[i][0][0]);
+        }
+      }
+    }
+    return keys;
+  }
+
+  values() {
+    const values = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!values.includes(this.keyMap[i][j][1])) {
+            values.push(this.keyMap[i][j][1]);
+          }
+        }
+      }
+    }
+    return values;
+  }
 }
 
 
 let ht = new HashTable(17);
-ht.set("maroon","#800000")
-ht.set("yellow","#FFFF00")
-ht.set("olive","#808000")
-ht.set("salmon","#FA8072")
-ht.set("lightcoral","#F08080")
-ht.set("mediumvioletred","#C71585")
-ht.set("plum","#DDA0DD")
-console.log(ht.get("lightcoral"))
-// console.log(ht)
+ht.set("maroon", "#800000");
+ht.set("yellow", "#FFFF00");
+ht.set("olive", "#808000");
+ht.set("salmon", "#FA8072");
+ht.set("lightcoral", "#F08080");
+ht.set("mediumvioletred", "#C71585");
+ht.set("plum", "#DDA0DD");
+ht.set("plum", "#DUPLICATE");
+// console.log(ht.keys());
+console.log(ht.values());
+
