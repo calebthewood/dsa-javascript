82eb4630a33955fea792bf15d20aea82e41ea6e2
calebthewood <calebwood.cs@gmail.com>
2022-09-14 16:18:36 -0700

    add min stack

--- algo-challenges/LeetCode.js
+++ algo-challenges/LeetCode.js
@@ -424,3 +424,52 @@ var threeSum = function(nums) {
     if (remainder) node.next = new ListNode(remainder)
     return output;
 };
+
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+var MinStack = function() {
+    this.stack = [];
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    if (this.stack.length < 1) {
+        this.stack.push([val, val])
+    } else {
+        let min = Math.min(val, this.stack[this.stack.length-1][1])
+        this.stack.push([val, min]);
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    const [val, min] = this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1][0]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    const [val, min] = this.stack[this.stack.length - 1]
+    return min;
+};
+
