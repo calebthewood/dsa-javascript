4b631ccec18bd18002991ac9fc7711dbc75fba7d
calebthewood <calebwood.cs@gmail.com>
2023-03-29 22:40:11 -0700

    Start 2.1

--- CTCI-Questions/Ch02-LinkedLists.js
+++ CTCI-Questions/Ch02-LinkedLists.js
@@ -13,14 +13,69 @@ class Node {
   }
 }
 
+/**
+ * @class Represents a linked list.
+ */
 class LinkedList {
-  constructor(val) {
-    this.head = null;
+  /**
+   * @constructor Creates a new linked list with the given value as the first node.
+   * @param {*} val - The value to be stored in the first node.
+   */
+  constructor(node) {
+    /**
+    * @type {Node}
+    * @default null
+    */
+    this.head = node;
+    /**
+    * @type {Node}
+    * @default null
+    */
     this.tail = null;
+
+    /**
+    * @type {number}
+    * @default 0
+    */
     this.length = 0;
   }
 
-  deleteNode() {
-
+  /**
+   * Deletes a node from the linked list.
+   *
+   * @param {Node} node - The node to be deleted.
+   * @returns {Node} - The deleted node, or null if the linked list is empty.
+   */
+  deleteNode(node) {
+    if (this.head === null) {
+      return null;
+    }
+    if (node === this.head) {
+      this.head = this.head.next;
+      if (this.head !== null) {
+        this.head.prev = null;
+      }
+      if (this.tail === node) {
+        this.tail = null;
+      }
+    } else {
+      let current = this.head.next;
+      while (current !== null) {
+        if (node === current) {
+          current.prev.next = current.next;
+          if (current.next !== null) {
+            current.next.prev = current.prev;
+          } else {
+            this.tail = current.prev;
+          }
+          current.next = null;
+          current.prev = null;
+          break;
+        }
+        current = current.next;
+      }
+    }
+    this.length--;
+    return node;
   }
 }
