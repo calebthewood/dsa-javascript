7e5bebdfb8cfe38c6c1f61fa58b00e364f83b7e4
calebthewood <calebwood.cs@gmail.com>
2023-03-30 11:58:46 -0700

    Add test case to remove dupes

--- CTCI-Questions/Ch02-LinkedLists.test.js
+++ CTCI-Questions/Ch02-LinkedLists.test.js
@@ -2,11 +2,12 @@ const { generateDupLL, LinkedList, Node } = require('./Ch02-LinkedLists');
 
 
 
-
-
 describe("2.1 Remove Duplicates", () => {
+  const listA = generateDupLL(20)
+  test("Should have initial length of 40", () => {
+    expect(listA.length).toEqual(40);
+  });
   test("Should have length of 20 after removing dups", () => {
-    const listA = generateDupLL(20)
     listA.removeDups()
     expect(listA.length).toEqual(20);
   });
